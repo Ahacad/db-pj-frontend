37e1f38b7981ecd313a3940cc5db32a993350afe
ahacad <1393345075@qq.com>
2021-03-26 19:50:25 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Header from "./Header";
 
 function App() {
+  const [text, setText] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log("START FETCHING");
+      const res = await axios
+        .get("http://localhost:4000/users")
+        .then((resp) => {
+          console.log(resp);
+          return resp.data[0].email;
+        });
+      console.log(res);
+      setText(res);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {text} edit <code>src/App.js</code> and save to {text} reload.
         </p>
         <a
           className="App-link"
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+export default function Header(props) {
+  return (
+    <div className="flex">
+      <a className="w-20">Home</a>
+      <a className="w-20 ml-4">Login</a>
+      <a className="w-20 ml-4">Register</a>
+    </div>
+  );
+}
