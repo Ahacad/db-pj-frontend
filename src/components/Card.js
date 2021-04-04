ccc406484dd9fb89c57096196bee8a655c0d1da7
ahacad <1393345075@qq.com>
2021-04-04 21:24:58 +0800

    feat(Main): add mocks

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,20 @@
 import { formatDistanceToNow } from "date-fns";
 
+function displayTime(date) {
+  // TODO: if time > 1 month, display absoulte time
+}
+
 export default function Card(props) {
   return (
-    <div className="w-72 shadow-xl">
-      {props.name},{" "}
-      {formatDistanceToNow(new Date(props.createTime), { addSuffix: true })}
+    <div className="w-2/5 h-32 pt-2 pl-2 mb-2 text-left border-b-2 border-gray-200 grid grid-cols-7 gap-1">
+      <div className="col-span-1">{props.name}</div>
+      <div className="col-span-5">
+        <div>{props.title}</div>
+      </div>
+      <div className="col-span-1">
+        {formatDistanceToNow(new Date(props.createTime), { addSuffix: true })}
+        {/* TODO: like, reply count */}
+      </div>
     </div>
   );
 }
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,10 +1,43 @@
 import Card from "components/Card";
+import axios from "axios";
+import { useEffect } from "react";
+
+function Foo() {
+  useEffect(() => {
+    const fetchData = async () => {
+      // TODO: fetch main panel post data
+    };
+  }, []);
+  return (
+    <>
+      <Card
+        name="user1"
+        createTime="2021-04-04T11:19:57.563Z"
+        title="Hello world, the first post"
+      />
+      <Card
+        name="user1"
+        createTime="2021-04-04T11:19:57.563Z"
+        title="Hello world, the first post"
+      />
+      <Card
+        name="user1"
+        createTime="2021-04-04T11:19:57.563Z"
+        title="Hello world, the first post"
+      />
+      <Card
+        name="user1"
+        createTime="2021-04-04T11:19:57.563Z"
+        title="Hello world, the first post"
+      />
+    </>
+  );
+}
 
 export default function Main() {
   return (
     <div>
-      MAIN
-      <Card name="hello things" createTime="2021-04-04T11:19:57.563Z" />
+      <Foo />
     </div>
   );
 }
