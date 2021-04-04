d8e3b620b66e3d0b199844074b75c3603f8a7754
ahacad <1393345075@qq.com>
2021-04-04 20:56:39 +0800

    feat(date-fns): add date module using date-fns

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,10 @@
+import { formatDistanceToNow } from "date-fns";
+
 export default function Card(props) {
-  return <div className="w-72 shadow-xl">{props.name}</div>;
+  return (
+    <div className="w-72 shadow-xl">
+      {props.name},{" "}
+      {formatDistanceToNow(new Date(props.createTime), { addSuffix: true })}
+    </div>
+  );
 }
--- src/views/Main.js
+++ src/views/Main.js
@@ -4,7 +4,7 @@ export default function Main() {
   return (
     <div>
       MAIN
-      <Card name="hello things" />
+      <Card name="hello things" createTime="2021-04-04T11:19:57.563Z" />
     </div>
   );
 }
