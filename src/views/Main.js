1a069d71d149328bbc3a802fc7be000357becb97
ahacad <1393345075@qq.com>
2021-04-21 11:40:53 +0800

    feat(main): add addPost icon to main panel

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,8 +1,20 @@
 import Card from "components/Card";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { Fab } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: "absolute",
+    bottom: theme.spacing(4),
+    right: theme.spacing(4),
+  },
+}));
 
 function Foo() {
+  const classes = useStyles();
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
@@ -23,6 +35,9 @@ function Foo() {
       {posts.map((post) => (
         <Card post={post} />
       ))}
+      <Fab color="primary" className={classes.fab}>
+        <AddIcon />
+      </Fab>
     </>
   );
 }
