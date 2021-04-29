cf52efac0f78a916c10bd8ef44f8a9f6db1fa09c
ahacad <1393345075@qq.com>
2021-04-29 18:39:01 +0800

    feat(views/home): export home in views index

--- src/views/index.js
+++ src/views/index.js
@@ -2,3 +2,4 @@ export { default as Main } from "./Main";
 export { default as Register } from "./Register";
 export { default as Login } from "./Login";
 export { default as Thread } from "./Thread";
+export { default as Home } from "./Home";
